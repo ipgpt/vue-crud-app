19a4485f2af2ddebbed805ca179c97b1c559d645
Petro Fedorenko <petewaitforletters@gmail.com>
2020-09-04 17:57:47 +0300

    feat: implement connection with local storage and adding new user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,15 @@
 import Table from "../components/Table";
 export default {
   name: "App",
+  mounted() {
+    if (localStorage.getItem("users")) {
+      try {
+        this.users = JSON.parse(localStorage.getItem("users"));
+      } catch (e) {
+        localStorage.removeItem("users");
+      }
+    }
+  },
   data() {
     return {
       users: [
@@ -23,14 +32,20 @@ export default {
   },
   methods: {
     deleteUser(email) {
-      this.users = this.users.filter(user => user.email !== email);
+      this.users = this.users.filter((user) => user.email !== email);
+      this.saveUsers();
     },
     addUser(user) {
-      console.log('add ', user);
+      this.users.push(user);
+      this.saveUsers();
     },
     editUser(user) {
-      console.log('edit ', user);
-    }
-  }
+      console.log("edit ", user);
+    },
+    saveUsers() {
+      const parsed = JSON.stringify(this.users);
+      localStorage.setItem("users", parsed);
+    },
+  },
 };
 </script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,27 +2,30 @@
   <div>
     <h2>User page</h2>
     <router-link to="/" tag="button">Back to Homepage</router-link>
-    <hr>
-    <button v-on:click="addUser('react')">1</button>
+    <hr />
     <button v-on:click="editUser('angular')">2</button>
-    <form>
+    <form @submit.prevent="onSubmit">
       <label>
         Name
-        <input type="text" />
-      </label><br />
+        <input type="text" v-model="name" />
+      </label>
+      <br />
       <label>
         Surname
-        <input type="text" />
-      </label><br />
+        <input type="text" v-model="surname" />
+      </label>
+      <br />
       <label>
         Phone
-        <input type="text" />
-      </label><br />
+        <input type="text" v-model="phone" />
+      </label>
+      <br />
       <label>
         Email
-        <input type="text" />
-      </label><br />
-      <input type="submit" value="Save"/>
+        <input type="text" v-model="email" />
+      </label>
+      <br />
+      <input type="submit" value="Save" />
     </form>
   </div>
 </template>
@@ -31,18 +34,59 @@
 import Home from "./Home";
 
 export default {
+  data() {
+    return {
+      users: [],
+      name: "",
+      surname: "",
+      phone: "",
+      email: "",
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("users")) {
+      try {
+        this.cats = JSON.parse(localStorage.getItem("users"));
+      } catch (e) {
+        localStorage.removeItem("users");
+      }
+    }
+  },
   methods: {
     addUser: Home.methods.addUser,
     editUser: Home.methods.editUser,
-  }
-}
+    saveUsers: Home.methods.saveUsers,
+    onSubmit() {
+      if (
+        this.name.trim() &&
+        this.surname.trim() &&
+        this.phone.trim() &&
+        this.email.trim()
+      ) {
+        const newUser = {
+          name: this.name,
+          surname: this.surname,
+          phone: this.phone,
+          email: this.email,
+        };
+        console.log(newUser);
+        console.log(this.addUser);
+        this.addUser(newUser);
+        this.name = "";
+        this.surname = "";
+        this.phone = "";
+        this.email = "";
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
 form {
-    width: 300px;
-    margin: 0 auto;
-    text-align: end;
+  width: 300px;
+  margin: 0 auto;
+  text-align: end;
 }
 input {
   margin: 5px;
