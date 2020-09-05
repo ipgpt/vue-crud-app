9a7d1d19976db8a9e4a4dd302e53e0eec1cf1063
Petro Fedorenko <petewaitforletters@gmail.com>
2020-09-05 21:34:59 +0300

    refactor: create user form component

---
+++ src/components/UserForm.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="main-content__item">
+    <div class="error-text" v-if="errorForm.length">
+      <b>Error(s):</b>
+      <ul class="error-text__list">
+        <li v-for="(error, i) in errorForm" :key="i">{{ error }}</li>
+      </ul>
+    </div>
+    <form
+      class="form"
+      @submit.prevent="$emit('on-submit', existingUser || userData, !!existingUser)"
+    >
+      <label>
+        Name
+        <input
+          class="form__field"
+          type="text"
+          pattern="^[a-zA-Z]+$"
+          placeholder="Tony"
+          v-if="existingUser"
+          v-model="existingUser.name"
+        />
+        <input
+          class="form__field"
+          type="text"
+          pattern="^[a-zA-Z]+$"
+          placeholder="Tony"
+          v-else
+          v-model="userData.name"
+        />
+      </label>
+      <br />
+      <label>
+        Surname
+        <input
+          class="form__field"
+          type="text"
+          pattern="^[a-zA-Z]+$"
+          placeholder="Hawk"
+          v-if="existingUser"
+          v-model="existingUser.surname"
+        />
+        <input
+          class="form__field"
+          type="text"
+          pattern="^[a-zA-Z]+$"
+          placeholder="Hawk"
+          v-else
+          v-model="userData.surname"
+        />
+      </label>
+      <br />
+      <label>
+        Phone
+        <input
+          class="form__field"
+          type="tel"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
+          placeholder="111-111-11-11"
+          v-if="existingUser"
+          v-model="existingUser.phone"
+        />
+        <input
+          class="form__field"
+          type="tel"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
+          placeholder="111-111-11-11"
+          v-else
+          v-model="userData.phone"
+        />
+      </label>
+      <br />
+      <label>
+        Email
+        <input
+          class="form__field"
+          type="email"
+          placeholder="1@1.1"
+          v-if="existingUser"
+          v-model="existingUser.email"
+        />
+        <input class="form__field" type="email" placeholder="1@1.1" v-else v-model="userData.email" />
+      </label>
+      <br />
+      <button class="button button-save" type="submit">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    errorForm: {
+      type: Array,
+      required: true,
+    },
+    existingUser: null || Object,
+    userData: Object,
+  },
+};
+</script>
+
+<style scoped>
+.error-text__list {
+  margin: 0;
+}
+.form {
+  width: 280px;
+  margin: 0 auto;
+  text-align: end;
+}
+.form__field {
+  margin: 5px 0;
+}
+.button-save {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -25,96 +25,20 @@
           ></textarea>
         </label>
         <button class="button button-import-json" @click="parseJSON">Import JSON</button>
-        </div>
-        <div class="main-content__item">
-          <div class="error-text" v-if="errorForm.length">
-            <b>Error(s):</b>
-            <ul class="error-text__list">
-              <li v-for="(error, i) in errorForm" :key="i">{{ error }}</li>
-            </ul>
-          </div>
-          <form class="form" @submit.prevent="onSubmit">
-            <label>
-              Name
-              <input
-                class="form__field"
-                type="text"
-                pattern="^[a-zA-Z]+$"
-                placeholder="Tony"
-                v-if="user"
-                v-model="user.name"
-              />
-              <input
-                class="form__field"
-                type="text"
-                pattern="^[a-zA-Z]+$"
-                placeholder="Tony"
-                v-else
-                v-model="name"
-              />
-            </label>
-            <br />
-            <label>
-              Surname
-              <input
-                class="form__field"
-                type="text"
-                pattern="^[a-zA-Z]+$"
-                placeholder="Hawk"
-                v-if="user"
-                v-model="user.surname"
-              />
-              <input
-                class="form__field"
-                type="text"
-                pattern="^[a-zA-Z]+$"
-                placeholder="Hawk"
-                v-else
-                v-model="surname"
-              />
-            </label>
-            <br />
-            <label>
-              Phone
-              <input
-                class="form__field"
-                type="tel"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
-                placeholder="111-111-11-11"
-                v-if="user"
-                v-model="user.phone"
-              />
-              <input
-                class="form__field"
-                type="tel"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
-                placeholder="111-111-11-11"
-                v-else
-                v-model="phone"
-              />
-            </label>
-            <br />
-            <label>
-              Email
-              <input
-                class="form__field"
-                type="email"
-                placeholder="1@1.1"
-                v-if="user"
-                v-model="user.email"
-              />
-              <input class="form__field" type="email" placeholder="1@1.1" v-else v-model="email" />
-            </label>
-            <br />
-            <button class="button button-save" type="submit">Save</button>
-          </form>
-        </div>
+      </div>
+      <UserForm
+        v-bind:errorForm="errorForm"
+        v-bind:existingUser="user"
+        v-bind:userData="userData"
+        @on-submit="onSubmit"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { nanoid } from "nanoid";
+import UserForm from "../components/UserForm";
 import Home from "./Home";
 import router from "../router";
 export default {
@@ -122,15 +46,18 @@ export default {
     return {
       users: [],
       user: null,
+      userData: {
+        name: "",
+        surname: "",
+        phone: "",
+        email: "",
+      },
       jsonData: null,
-      name: "",
-      surname: "",
-      phone: "",
-      email: "",
       errorJSON: "",
       errorForm: [],
     };
   },
+  components: { UserForm },
   mounted() {
     if (localStorage.getItem("users")) {
       try {
@@ -155,107 +82,76 @@ export default {
       localStorage.removeItem("editUser");
       router.push("/");
     },
+    IsJsonString(str) {
+      try {
+        JSON.parse(str);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    },
     parseJSON() {
-      const dataFromJSON = Array.isArray(this.jsonData)
+      const dataFromJSON = this.IsJsonString(this.jsonData)
         ? JSON.parse(this.jsonData)
         : this.jsonData;
+      const type = typeof dataFromJSON;
       if (
-        Array.isArray(this.jsonData) &&
+        type !== "string" &&
+        Array.isArray(dataFromJSON) &&
         String(dataFromJSON[0]) === "[object Object]"
       ) {
-        this.name = dataFromJSON[0].name || this.name;
-        this.surname = dataFromJSON[0].surname || this.surname;
-        this.phone = dataFromJSON[0].phone || this.phone;
-        this.email = dataFromJSON[0].email || this.email;
+        this.userData.name = dataFromJSON[0].name || this.userData.name;
+        this.userData.surname =
+          dataFromJSON[0].surname || this.userData.surname;
+        this.userData.phone = dataFromJSON[0].phone || this.userData.phone;
+        this.userData.email = dataFromJSON[0].email || this.userData.email;
         this.jsonData = null;
         this.errorJSON = "";
-      } else if (dataFromJSON) {
+      } else if (String(dataFromJSON).length) {
         this.errorJSON = "Error: Wrong structure of JSON!";
       } else {
         this.errorJSON = "Error: JSON is empty!";
       }
     },
-    checkFormAddUser() {
+    checkForm(user) {
       if (
-        this.name.trim() &&
-        this.surname.trim() &&
-        this.phone.trim() &&
-        this.email.trim()
+        user.name.trim() &&
+        user.surname.trim() &&
+        user.phone.trim() &&
+        user.email.trim()
       ) {
         return true;
       }
-
       this.errorForm = [];
-
-      if (!this.name) {
+      if (!user.name) {
         this.errorForm.push("Name required.");
       }
-      if (!this.surname) {
+      if (!user.surname) {
         this.errorForm.push("Surname required.");
       }
-      if (!this.phone) {
+      if (!user.phone) {
         this.errorForm.push("Phone required.");
       }
-      if (!this.email) {
+      if (!user.email) {
         this.errorForm.push("Email required.");
       }
     },
-    checkFormEditUser() {
-      if (
-        this.user.name.trim() &&
-        this.user.surname.trim() &&
-        this.user.phone.trim() &&
-        this.user.email.trim()
-      ) {
-        return true;
-      }
-
-      this.errorForm = [];
-
-      if (!this.user.name) {
-        this.errorForm.push("Name required.");
-      }
-      if (!this.user.surname) {
-        this.errorForm.push("Surname required.");
-      }
-      if (!this.user.phone) {
-        this.errorForm.push("Phone required.");
-      }
-      if (!this.user.email) {
-        this.errorForm.push("Email required.");
-      }
-    },
-    onSubmit() {
-      if (this.checkFormAddUser()) {
+    onSubmit(savedUser, editStatus) {
+      if (this.checkForm(savedUser)) {
         this.errorForm = "";
-        const newUser = {
-          id: nanoid(),
-          name: this.name,
-          surname: this.surname,
-          phone: this.phone,
-          email: this.email,
+        savedUser.id = editStatus ? this.user.id : nanoid();
+        if (editStatus) {
+          this.editUser(savedUser);
+          localStorage.removeItem("user");
+        } else {
+          this.addUser(savedUser);
+        }
+        this.userData = {
+          name: "",
+          surname: "",
+          phone: "",
+          email: "",
         };
-        this.addUser(newUser);
-        this.name = "";
-        this.surname = "";
-        this.phone = "";
-        this.email = "";
-        this.moveToHomePage();
-      } else if (this.user && this.checkFormEditUser()) {
-        this.errorForm = "";
-        const editedUser = {
-          id: this.user.id,
-          name: this.user.name,
-          surname: this.user.surname,
-          phone: this.user.phone,
-          email: this.user.email,
-        };
-        this.editUser(editedUser);
-        localStorage.removeItem("user");
-        this.user.name = "";
-        this.user.surname = "";
-        this.user.phone = "";
-        this.user.email = "";
         this.moveToHomePage();
       }
     },
@@ -263,7 +159,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .error-text {
   width: 280px;
   margin: 0 auto;
@@ -271,9 +167,6 @@ export default {
   padding: 5px 0;
   background: blue;
   border: 1px solid white;
-}
-.error-text__list {
-  margin: 0;
 }
 .json-textarea {
   width: 250px;
@@ -288,14 +181,6 @@ export default {
   background-color: inherit;
   color: inherit;
 }
-.form {
-  width: 280px;
-  margin: 0 auto;
-  text-align: end;
-}
-.form__field {
-  margin: 5px 0;
-}
 .json-textarea::placeholder,
 .form__field::placeholder {
   color: mediumspringgreen;
@@ -303,10 +188,6 @@ export default {
 .button-import-json {
   width: 280px;
   margin: 15px 0;
-}
-.button-save {
-  width: 100%;
-  margin-top: 10px;
 }
 @media screen and (min-width: 600px) {
   .main-content {
